4e59d53c6a85a5ba140d84653a4a5ef6c66911c0
shirin <shirinanlen@gmail.com>
2020-06-10 14:55:01 -0400

    transitions

--- Instructions.js
+++ Instructions.js
@@ -3,6 +3,102 @@ export default class Instructions {
     this.handleClick = this.handleClick.bind(this);
     this.el = document.createElement('div');
     this.goToNext = goToNext;
+    this.delay = this.delay.bind(this);
+    this.pageTransition = this.pageTransition.bind(this);
+    this.contentAnimation = this.contentAnimation.bind(this);
+    this.initAnimation = this.initAnimation.bind(this);
+    this.setTimeout();
+  }
+
+  handleClick() {
+    console.log('go to next page');
+    this.goToNext();
+  }
+
+  setTimeout() {
+    window.setTimeout(() => {
+      this.contentAnimation()
+    }, 100);
+  }
+
+  delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+      setTimeout(() => {
+        done();
+      }, n);
+    });
+  }
+
+  pageTransition() {
+    console.log('clock')
+    let tl = gsap.timeline();
+    tl.to(".loading-screen", {
+      duration: 1.5,
+      opacity: 0,
+      delay: 0.1
+    });
+    // tl.to(".loading-screen", {
+    //   opacity: 1,
+    //   delay: 0.5,
+    //   duration: 1.5,
+    // });
+    tl.set(".loading-screen", { opacity: 0 })
+  }
+
+  contentAnimation() {
+    let tl = gsap.timeline();
+    // tl.from(".animate-this", {
+    //   duration: 0.6,
+    //   y: -140,
+    //   opacity: 0,
+    //   stragger: 0.9,
+    //   delay: 0.3
+    // });
+    tl.to(".animate-this", {
+      duration: 0.7,
+      opacity: 1,
+      stragger: 0.9,
+      // y: -60,
+      // delay: 0.3
+    });
+    // tl.from(".animate-text", {
+    //   duration: 1.2,
+    //   opacity: 0
+    // });
+    // tl.to(".animate-text", {
+    //   opacity: 0.7,
+    //   duration: 0.2,
+    // });
+  }
+
+  initAnimation() {
+    console.log('click')
+    barba.init({
+      sync: true,
+      transitions: [
+        {
+          async leave(data) {
+
+            const done = this.async();
+            this.pageTransition();
+            await this.delay(1000);
+            done();
+          },
+
+          async enter(data) {
+            console.log('done');
+
+            this.contentAnimation();
+          },
+
+          async once(data) {
+            this.contentAnimation();
+          }
+        },
+      ],
+
+    })
   }
 
   handleClick() {
@@ -12,22 +108,24 @@ export default class Instructions {
 
   render() {
     this.el.innerHTML = `
-    
+    <main data-barba="container" data-barba-namespace="instruction-section">
       <div class="InstructionPage">
-      <div class="bgInstruction">
-      <p class='homeInstructionTwo' > <span class="before"> Before starting</span> </br></br> Add the app to your mobile home-screen <strong>*</strong>.</br> 
-      Then, activate the app in advance to best protect you and your contacts.</p>
-      <button class="download"></button>
-      <div>
-        <h5 class="homeInstructionThree"><strong>*</strong> ANDROID USERS: look for this icon-> 
-        <img class="icon" src="./images/androidAddHomeScreen.svg" alt="android" width="14" height="18"/>- then slide to the right and “Add to Home Screen.” 
+       <div class="bgInstruction animate-this">
+         <p class='homeInstructionTwo animate-text' > <span class="before"> Before starting</span> </br></br> Add the app to your mobile home-screen <strong>*</strong>.</br> 
+          Then, activate the app in advance to best protect you and your contacts.</p>
+         <button class="download animate-text"></button>
+       <div class= "small animate-text">
+       <h5 class="homeInstructionThree"><strong>*</strong> ANDROID USERS: look for this icon-> 
+       <img class="icon" src="./images/androidAddHomeScreen.svg" alt="android" width="14" height="18"/>- then slide to the right and “Add to Home Screen.” 
         </br>
         <strong>*</strong> IOS USERS: look for this icon-> 
-        <img class="icon" src="./images/iosAddHomeScreen.svg" alt="ios" width="18" height="18" />- then tap Settings on the top right and “Add to Home Screen.”</h5>
+       <img class="icon" src="./images/iosAddHomeScreen.svg" alt="ios" width="18" height="18" />- then tap Settings on the top right and “Add to Home Screen.”</h5>
       </div>
-      </div>
+     </div>
     </div>
-    
+    <button class="demo">next</button>
+
+    </main>
     `;
 
     this.el.querySelector('button').addEventListener('click', this.handleClick)
--- Welcome.js
+++ Welcome.js
@@ -3,6 +3,11 @@ export default class Welcome {
     this.handleClick = this.handleClick.bind(this);
     this.el = document.createElement('div');
     this.goToNext = goToNext;
+    this.delay = this.delay.bind(this);
+    this.pageTransition = this.pageTransition.bind(this);
+    this.contentAnimation = this.contentAnimation.bind(this);
+    this.initAnimation = this.initAnimation.bind(this);
+    this.setTimeout();
   }
 
   handleClick() {
@@ -10,10 +15,95 @@ export default class Welcome {
     this.goToNext();
   }
 
+  setTimeout(){
+    window.setTimeout(() => {
+      this.pageTransition();
+      this.contentAnimation();
+      window.setTimeout(() => {
+        this.goToNext();
+      }, 3200)
+    }, 1000);
+  }
+
+  delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+      setTimeout(() => {
+        done();
+      }, n);
+    });
+  }
+
+  pageTransition() {
+    console.log('clock')
+    let tl = gsap.timeline();
+    tl.to(".loading-screen", {
+      duration: 1.8,
+      width: "100%",
+      top: "0",
+      ease: "Expo.easeInOut"
+    });
+
+    tl.to(".loading-screen", {
+      duration: 2,
+      width: "100%",
+      top: "100%",
+      ease: "Expo.easeInOut",
+      // delay: 0.1
+    });
+    tl.set(".loading-screen", {top: "-100%"})
+  }
+
+  contentAnimation() {
+    let tl = gsap.timeline();
+    tl.from(".animate-this", {
+      duration: 1,
+      y: 1,
+      opacity: 1,
+      stragger: 0.9,
+      delay: 1
+    });
+    tl.to(".animate-this", {
+      opacity: 0,
+      y: 50,
+    })
+  }
+
+initAnimation(){
+  console.log('click')
+  barba.init({
+    sync: true,
+    transitions: [
+      {
+        async leave(data) {
+
+          const done = this.async();
+          this.pageTransition();
+          await this.delay(1000);
+          done();
+        },
+
+        async enter(data) {
+          console.log('done');
+
+          this.contentAnimation();
+        },
+
+        async once(data) {
+          this.contentAnimation();
+        } 
+      },
+    ],
+
+  })
+}
+
   render() {
     this.el.innerHTML = `
-      <div class="homePage">
-      <div class="textHome">
+    <main data-barba="container" data-barba-namespace="home-section">
+
+      <div class="animationPage">
+      <div class="textHome animate-this">
         <p class='homeInstructionOne'> Contact emergency friends & local bail funds </br>at the push of a button
        </br> <strong>in case</strong></p>
         <h1 class="shit">SHIT</h1>
@@ -21,15 +111,14 @@ export default class Welcome {
         <h1 class="down">DOWN</h1>
       </div>
     </div>
-    <button class="demo">next</button>
+    </main>
     `;
 
-    this.el.querySelector('button').addEventListener('click', this.handleClick)
-
+  
     return this.el;
   }
 
   unmount() {
-    this.el.querySelector('button').removeEventListener('click', this.handleClick);
+   
   }
 }
