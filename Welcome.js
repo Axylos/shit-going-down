ec8234cbcdf3b20bbc164e639fda113f1a3f2860
Drake Talley <robertdraketalley@gmail.com>
2020-07-31 20:27:52 -0400

    stuff

--- Welcome.js
+++ Welcome.js
@@ -139,6 +139,19 @@ export default class Welcome {
       <div class="loginSection">
           <div class"loginDiv">
             <div class="buttonDiv">
+  <div class="fb-login-button" 
+    data-max-rows="2" 
+    data-size="large" 
+    data-button-type="login_with"
+    data-auto-logout-link="false" 
+    data-use-continue-as="true" 
+    data-scope="public_profile,email"
+    onlogin="checkLoginState">
+    <button class="btnLogin sms" onclick="checkLoginState()">
+      <span class='loginInstruction'>to manuly save &amp; SMS friends.</span></br>
+    </button>
+  </div>
+
               <button class="btnLogin sms" onclick="location.href='/sms'">
                 <span class='loginInstruction'>send SMS</span></br>
               </button>
